f1c293754a1ce71953aad11fea6647f9e30be9ba
Harsh Kothari <harshkothari410@gmail.com>
2015-11-02 18:43:55 -0500

    initial commit

---
+++ hma/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect, HttpResponse, Http404
+from django.core.exceptions import PermissionDenied
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+
+from django.dispatch import receiver
+from django.db import models
+from django.db.models import F, Q
+from django.core.files.storage import default_storage as storage
+
+def index(request):
+	return render( request, 'index.html', locals() )
