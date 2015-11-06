872d0f48b9fc2726101784b30462717eb10cb193
Rohit Bobade <Rohit@Rohits-MBP.fios-router.home>
2015-11-06 13:08:27 -0500

    tweet.js

--- static/js/tweet.js
+++ static/js/tweet.js
@@ -2,217 +2,217 @@ window.tweet = {
  "states":[
     {
         "url": "/location/california/", 
-        "tweet_count": 0, 
+        "tweet_count": 1090, 
         "type": "s", 
         "name": "California", 
         "population": 1213
     }, 
     {
         "url": "/location/new_jersey/", 
-        "tweet_count": 0, 
+        "tweet_count": 899, 
         "type": "s", 
         "name": "New Jersey", 
         "population": 0
     }, 
     {
         "url": "/location/new_york/", 
-        "tweet_count": 100, 
+        "tweet_count": 1134, 
         "type": "s", 
         "name": "New York", 
         "population": 0
     }, 
     {
         "url": "/location/alabama/", 
-        "tweet_count": 7856, 
+        "tweet_count": 89, 
         "type": "s", 
         "name": "Alabama", 
         "population": 4779736
     }, 
     {
         "url": "/location/alaska/", 
-        "tweet_count": 4589, 
+        "tweet_count": 68, 
         "type": "s", 
         "name": "Alaska", 
         "population": 710231
     }, 
     {
         "url": "/location/arizona/", 
-        "tweet_count": 78569, 
+        "tweet_count": 487, 
         "type": "s", 
         "name": "Arizona", 
         "population": 6392017
     }, 
     {
         "url": "/location/arkansas/", 
-        "tweet_count": 7856, 
+        "tweet_count": 57, 
         "type": "s", 
         "name": "Arkansas", 
         "population": 2915918
     }, 
     {
         "url": "/location/colorado/", 
-        "tweet_count": 45896, 
+        "tweet_count": 587, 
         "type": "s", 
         "name": "Colorado", 
         "population": 5029196
     }, 
     {
         "url": "/location/connecticut/", 
-        "tweet_count": 12456, 
+        "tweet_count": 400, 
         "type": "s", 
         "name": "Connecticut", 
         "population": 3574097
     }, 
     {
         "url": "/location/delaware/", 
-        "tweet_count": 9856, 
+        "tweet_count": 879, 
         "type": "s", 
         "name": "Delaware", 
         "population": 897934
     }, 
     {
         "url": "/location/florida/", 
-        "tweet_count": 9856, 
+        "tweet_count": 689, 
         "type": "s", 
         "name": "Florida", 
         "population": 18801310
     }, 
     {
         "url": "/location/georgia/", 
-        "tweet_count": 8956, 
+        "tweet_count": 569, 
         "type": "s", 
         "name": "Georgia\t", 
         "population": 9687653
     }, 
     {
         "url": "/location/hawaii/", 
-        "tweet_count": 5694, 
+        "tweet_count": 500, 
         "type": "s", 
         "name": "Hawaii", 
         "population": 1360301
     }, 
     {
         "url": "/location/idaho/", 
-        "tweet_count": 56984, 
+        "tweet_count": 800, 
         "type": "s", 
         "name": "Idaho", 
         "population": 1567582
     }, 
     {
         "url": "/location/illinois/", 
-        "tweet_count": 54893, 
+        "tweet_count": 89, 
         "type": "s", 
         "name": "Illinois", 
         "population": 12830632
     }, 
     {
         "url": "/location/indiana/", 
-        "tweet_count": 5167, 
+        "tweet_count": 4, 
         "type": "s", 
         "name": "Indiana", 
         "population": 6483802
     }, 
     {
         "url": "/location/iowa/", 
-        "tweet_count": 78466, 
+        "tweet_count": 45, 
         "type": "s", 
         "name": "Iowa", 
         "population": 3046355
     }, 
     {
         "url": "/location/kansas/", 
-        "tweet_count": 4566, 
+        "tweet_count": 24, 
         "type": "s", 
         "name": "Kansas", 
         "population": 2853118
     }, 
     {
         "url": "/location/kentucky/", 
-        "tweet_count": 6975, 
+        "tweet_count": 34, 
         "type": "s", 
         "name": "Kentucky", 
         "population": 4339367
     }, 
     {
         "url": "/location/louisiana/", 
-        "tweet_count": 4569, 
+        "tweet_count": 56, 
         "type": "s", 
         "name": "Louisiana", 
         "population": 4533372
     }, 
     {
         "url": "/location/maine/", 
-        "tweet_count": 5479, 
+        "tweet_count": 59, 
         "type": "s", 
         "name": "Maine", 
         "population": 1328361
     }, 
     {
         "url": "/location/maryland/", 
-        "tweet_count": 69876, 
+        "tweet_count": 569, 
         "type": "s", 
         "name": "Maryland", 
         "population": 5773552
     }, 
     {
         "url": "/location/massachusetts/", 
-        "tweet_count": 12548, 
+        "tweet_count": 600, 
         "type": "s", 
         "name": "Massachusetts", 
         "population": 6547629
     }, 
     {
         "url": "/location/michigan/", 
-        "tweet_count": 56987, 
+        "tweet_count": 297, 
         "type": "s", 
         "name": "Michigan", 
         "population": 9883640
     }, 
     {
         "url": "/location/minnesota/", 
-        "tweet_count": 698532, 
+        "tweet_count": 23, 
         "type": "s", 
         "name": "Minnesota", 
         "population": 5303925
     }, 
     {
         "url": "/location/mississippi/", 
-        "tweet_count": 45392, 
+        "tweet_count": 82, 
         "type": "s", 
         "name": "Mississippi", 
         "population": 2967297
     }, 
     {
         "url": "/location/missouri/", 
-        "tweet_count": 66587, 
+        "tweet_count": 19, 
         "type": "s", 
         "name": "Missouri", 
         "population": 5988927
     }, 
     {
         "url": "/location/montana/", 
-        "tweet_count": 3658, 
+        "tweet_count": 476, 
         "type": "s", 
         "name": "Montana", 
         "population": 989415
     }, 
     {
         "url": "/location/nebraska/", 
-        "tweet_count": 5246, 
+        "tweet_count": 494, 
         "type": "s", 
         "name": "Nebraska", 
         "population": 1826341
     }, 
     {
         "url": "/location/nevada/", 
-        "tweet_count": 45879, 
+        "tweet_count": 233, 
         "type": "s", 
         "name": "Nevada", 
         "population": 2700551
     }, 
     {
         "url": "/location/new_hampshire/", 
-        "tweet_count": 12457, 
+        "tweet_count": 367, 
         "type": "s", 
         "name": "New Hampshire", 
         "population": 1316470
@@ -226,49 +226,49 @@ window.tweet = {
     }, 
     {
         "url": "/location/north_carolina/", 
-        "tweet_count": 866594, 
+        "tweet_count": 106, 
         "type": "s", 
         "name": "North Carolina", 
         "population": 9535483
     }, 
     {
         "url": "/location/north_dakota/", 
-        "tweet_count": 6589, 
+        "tweet_count": 304, 
         "type": "s", 
         "name": "North Dakota", 
         "population": 672591
     }, 
     {
         "url": "/location/ohio/", 
-        "tweet_count": 789655, 
+        "tweet_count": 202, 
         "type": "s", 
         "name": "Ohio", 
         "population": 11536504
     }, 
     {
         "url": "/location/oklahoma/", 
-        "tweet_count": 54896, 
+        "tweet_count": 189, 
         "type": "s", 
         "name": "Oklahoma", 
         "population": 3751351
     }, 
     {
         "url": "/location/oregon/", 
-        "tweet_count": 86954, 
+        "tweet_count": 209, 
         "type": "s", 
         "name": "Oregon", 
         "population": 3831074
     }, 
     {
         "url": "/location/pennsylvania/", 
-        "tweet_count": 5439875, 
+        "tweet_count": 800, 
         "type": "s", 
         "name": "Pennsylvania", 
         "population": 12702379
     }, 
     {
         "url": "/location/rhode_island/", 
-        "tweet_count": 85642, 
+        "tweet_count": 300, 
         "type": "s", 
         "name": "Rhode Island", 
         "population": 1052567
@@ -282,42 +282,42 @@ window.tweet = {
     }, 
     {
         "url": "/location/south_dakota/", 
-        "tweet_count": 69854, 
+        "tweet_count": 789, 
         "type": "s", 
         "name": "South Dakota", 
         "population": 814180
     }, 
     {
         "url": "/location/tennessee/", 
-        "tweet_count": 9657, 
+        "tweet_count": 688, 
         "type": "s", 
         "name": "Tennessee", 
         "population": 6346105
     }, 
     {
         "url": "/location/texas/", 
-        "tweet_count": 9874562, 
+        "tweet_count": 987, 
         "type": "s", 
         "name": "Texas", 
         "population": 25145561
     }, 
     {
         "url": "/location/utah/", 
-        "tweet_count": 4235, 
+        "tweet_count": 834, 
         "type": "s", 
         "name": "Utah", 
         "population": 2763885
     }, 
     {
         "url": "/location/vermont/", 
-        "tweet_count": 2564, 
+        "tweet_count": 779, 
         "type": "s", 
         "name": "Vermont", 
         "population": 625741
     }, 
     {
         "url": "/location/virginia/", 
-        "tweet_count": 4597, 
+        "tweet_count": 839, 
         "type": "s", 
         "name": "Virginia", 
         "population": 8001024
@@ -331,21 +331,21 @@ window.tweet = {
     }, 
     {
         "url": "/location/west_virginia/", 
-        "tweet_count": 2568, 
+        "tweet_count": 986, 
         "type": "s", 
         "name": "West Virginia", 
         "population": 1852994
     }, 
     {
         "url": "/location/wisconsin/", 
-        "tweet_count": 24698, 
+        "tweet_count": 400, 
         "type": "s", 
         "name": "Wisconsin", 
         "population": 5686986
     }, 
     {
         "url": "/location/wyoming/", 
-        "tweet_count": 6947, 
+        "tweet_count": 437, 
         "type": "s", 
         "name": "Wyoming", 
         "population": 563626
